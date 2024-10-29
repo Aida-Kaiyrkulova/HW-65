141e90e85a21c1bce12375e212f09d32df50b3f1
Aida-Kaiyrkulova <aida-kaiyr@yandex.ru>
2024-10-29 21:13:00 +0600

    App init

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <CssBaseline />
+            <App />
+        </BrowserRouter>
+    </StrictMode>,
+);
